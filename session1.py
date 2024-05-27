3ab33cc72fa869e7d9c5651b7005f306127f3607
rabibna <230822@students.aih.nsw.edu.au>
2024-05-27 18:37:27 +1000

    Update session1.py

--- session1.py
+++ session1.py
@@ -1,1 +1,42 @@
+# first program
+# Calculate area of circle
+# Area = πr2
+# Area of circle = pie*radius**2
 
+pie = 3.1415
+diam = 14
+radi = diam/2
+
+input pie value
+input radius value
+Calculate area by:
+ pie times r square
+dfisplay area of circle
+
+input miles value
+input name of the user
+convert kms into miles by:
+ miles is 1.6 * kms
+display kms with name
+
+
+# Write a program to input age of user and let them know if they have diabetes 
+# risk or not. use case is that a user with age more than 65 is risky else not
+# risky
+age = 65
+if age>= 65
+  print("This is risky")
+else
+ print("not risky")
+
+# Write a program to calculate the gst tax of two products, apple and pie
+# apple has 10% gst and pie has only 8%
+# Calculate total gst that need to be deducted from the bill
+# apple gross sales is $75, pie gross sale is $100
+
+apple = 75$
+pie = 100$
+gst_apple = 0.1*apple 
+gst_pie = 0.08*pie 
+print(gst_apple)
+print(gst_pie)
